212d4a354d21d11c47af88d7f1ba2ca1fa619730
Alex Pearson <pear7@pdx.edu>
2016-05-01 21:33:14 -0700

    Implemented Inverse S-box and updated S-box test bench to use the new tester class

--- test/bench/SubBytesTestBench.sv
+++ test/bench/SubBytesTestBench.sv
@@ -10,70 +10,32 @@ module SubBytesTestBench();
 // Input and Output connections
 logic [127:0] in, inInv, out, outInv;
 
-test_t qTests[$];
-
-// Test creation task
-task automatic MakeTest(bit [127:0] plain, bit [127:0] encrypted);
-begin
-  test_t newTest;
-  $cast(newTest.plain, plain);
-  $cast(newTest.encrypted, encrypted);
-  qTests.push_back(newTest);
-end
-endtask
-
 // Module declaration
 SubBytes Dut(in, out);
-//SubBytesInverse Dut2(inInv, outInv);
+SubBytesInverse Dut2(inInv, outInv);
 
 // Test exectuion and verfication task
-task ApplyTests();
-  test_t curTest;
-  bit [127:0] curOut, curOutInv;
+test_t curTest;
+bit [127:0] curOut, curOutInv;
 
-  while(qTests.size() != 0)
+initial
+begin
+  UnitTester tester;
+  tester = new();
+  tester.ParseFileForTestCases("test/vectors/fips_example_vectors.txt", "s_box");
+
+  while(tester.NumTests() != 0)
   begin
-    curTest = qTests.pop_front();
+    curTest = tester.GetNextTest();
     in = curTest.plain;
     inInv = curTest.encrypted;
     #1 repeat(1);
     curOut = out;
     curOutInv = outInv;
-    if(curOut !== curTest.encrypted)
-    begin
-      $display("*** Error: Current output doesn't match expected");
-      $display("***        Input:    %h", curTest.plain);
-      $display("***        Output:   %h", curOut);
-      $display("***        Expected: %h", curTest.encrypted);
-      $finish();
-    end
-    if(curOutInv !== curTest.plain)
-    begin
-
-    end
+    tester.Compare(in, curOut, curTest, 0);
+    tester.Compare(inInv, curOutInv, curTest, 1);
   end
-endtask
 
-int testFile, i;
-bit [127:0] parse1, parse2;
-string phaseString, tempString;
-
-initial
-begin
-  testFile = $fopen("test/vectors/fips_example_vectors.txt", "r");
-  while(!$feof(testFile))
-  begin
-    i = $fscanf(testFile, "%s %h\n", phaseString, parse1);
-    tempString = phaseString.substr(phaseString.len()-5, phaseString.len()-1);
-    if(tempString.icompare("start") == 0)
-    begin
-      i = $fscanf(testFile, "%s %h\n", phaseString, parse2);
-      MakeTest(parse1, parse2);
-    end
-  end
-  $fclose(testFile);
-
-  ApplyTests();
   $finish();
 end
 
