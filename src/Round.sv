d34c80ae7cd6f265c0a96fb66bb6d789e8baa799
Alex Pearson <pear7@pdx.edu>
2016-05-11 00:15:20 -0700

    Initial work to support a testbench for the Round module. Having issues with inverse, regular testing, and getting the correct data into the test cases remain.

--- src/Round.sv
+++ src/Round.sv
@@ -19,13 +19,13 @@ ShiftRows sr(wires[0], wires[1]);
 if(finalRound)
   assign wires[2] = wires[1];
 else
-  MixColumns(wires[1], wires[2]);
+  MixColumns mc(wires[1], wires[2]);
 
 AddRoundKey ark(wires[2], key, out);
 
 endmodule : Round
 
-module InverseRound(input state_t in, roundKey_t key,
+module RoundInverse(input state_t in, roundKey_t key,
                     output state_t out);
 parameter Round = 1;
 localparam finalRound = (Round == `NUM_ROUNDS) ? 1 : 0;
@@ -33,14 +33,14 @@ localparam finalRound = (Round == `NUM_ROUNDS) ? 1 : 0;
 // State wire array
 state_t wires[3];
 
-InverseShiftRows isr(in, wires[0]);
-InverseSubBytes isb(wires[0], wires[1]);
-AddRoundKey ark(wires[1], wires[2]);
+ShiftRowsInverse isr(in, wires[0]);
+SubBytesInverse isb(wires[0], wires[1]);
+AddRoundKey ark(wires[1], key, wires[2]);
 
 // Final round doesn't perform the mix columns step
 if(finalRound)
   assign out = wires[2];
 else
-  MixColumns(wires[2], out);
+  MixColumnsInverse mc(wires[2], out);
 
-endmodule : InverseRound
+endmodule : RoundInverse
--- test/bench/RoundTestBench.sv
+++ test/bench/RoundTestBench.sv
@@ -12,7 +12,7 @@ logic [`KEY_SIZE-1:0] key;
 
 // Module declaration
 Round Dut(in, key, out);
-RoundInverse Dut2(inInv, key, outInv);
+//RoundInverse Dut2(inInv, key, outInv);
 
 // Test execution and verification task
 keyTest_t curTest;
@@ -20,21 +20,27 @@ bit [127:0] curOut, curOutInv;
 
 initial
 begin
-  UnitKeyTester tester;
+  RoundTester tester;
   tester = new();
-  tester.ParseFileForTestCases("test/vectors/fips_example_vectors.txt", "k_sch");
+  tester.ParseFileForTestCases("test/vectors/fips_example_vectors.txt");
+  //tester.ParseFileForTestCases("test/vectors/input.txt");
+
+  // TODO: Figure out how to special case the last iteration of the algorithm
+  // that doesn't do the mix columns
+  curTest = tester.GetNextTest();
 
   while(tester.NumTests() != 0)
   begin
     curTest = tester.GetNextTest();
     in = curTest.plain;
-    inInv = curTest.encrypted;
+    //inInv = curTest.encrypted;
     key = curTest.roundKey;
+    //$display("%h, %h, %h", curTest.plain, curTest.encrypted, curTest.roundKey);
     #1 repeat(1);
     curOut = out;
-    curOutInv = outInv;
+    //curOutInv = outInv;
     tester.Compare(in, curOut, curTest, 0);
-    tester.Compare(inInv, curOutInv, curTest, 1);
+    //tester.Compare(inInv, curOutInv, curTest, 1);
   end
 
   $finish();
