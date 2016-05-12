29304e7a99c7a74abe4a8794c122f9840b2e3864
Scott Lawson <SRLawsonPDX@gmail.com>
2016-05-12 01:48:29 -0700

    KeyExpansion: Added algorithmic components for 192- and 256-bit keys
    The currently planned usage of this module is not going to work. See
    note in KeyExpansion.sv header, or commit diff.

--- src/ExpandKey.sv
+++ src/ExpandKey.sv
@@ -2,42 +2,64 @@
 // Key Expansion Module
 //
 
-// TODO: this module only supports 128-bit key - update to support larger keys
+// TODO: The original plan for this module isn't going to work for 192- and 256-bit keys. 
+// 
+// There is not a 1:1 correspondence between the iteration used to generate the key schedule and the
+// rounds for Nk != 16. For example, in AES 192, each iteration of the key expansion algorithm
+// generates 192 bits, and it is repeated 8 times, creating a vectore that is 216 bytes wide (the
+// last 8 of which are truncated). The round keys are every 128-bit (16-byte) block within this
+// vector (leaving the last 8 bytes unused). 
+// 
+// Thus, each round key may actually require the results from two separate iterations of the key
+// expansion algorithm. We need to move this module outside of the round modules, let it generate
+// the whole key, and use exensive buffering to get the right round keys in the right places.
+
 `include "AESDefinitions.svpkg"
 
 module KeyExpansion(input byte_t round, expandedKey_t prevExpandedKey,
                     output roundKey_t roundKey, expandedKey_t nextExpandedKey);
 
-localparam byte_t RCON[16] = '{
-    8'h8d, 8'h01, 8'h02, 8'h04, 8'h08, 8'h10, 8'h20, 8'h40, 
-    8'h80, 8'h1b, 8'h36, 8'h6c, 8'hd8, 8'hab, 8'h4d, 8'h9a
+localparam byte_t RCON[11] = '{
+    8'h8d, 8'h01, 8'h02, 8'h04, 8'h08, 8'h10, 
+    8'h20, 8'h40, 8'h80, 8'h1b, 8'h36, 
 };
 
 
 always_comb
 begin
 
-    nextExpandedKey.columns[0] = genFirstColumn(
-        round, prevExpandedKey.columns[0], prevExpandedKey.columns[KEY_NUM_COLS-1]);
+    nextExpandedKey.columns[0] = prevExpandedKey.columns[0] ^ ApplyRcon(
+                                    round, SubBytes_4(Rot(prevExpandedKey.columns[KEY_NUM_COLS-1])));
 
-    for (int i=1; i<=3; ++i)
-        nextExpandedKey.columns[i] = nextExpandedKey.columns[i-1] ^ prevExpandedKey.columns[i];
+    `ifndef AES_256 // Expanding a 128- or 192-bit key
+        for (int i=1; i<=(KEY_NUM_COLS-1); ++i)
+            nextExpandedKey.columns[i] = nextExpandedKey.columns[i-1] ^ prevExpandedKey.columns[i];
 
-    // this won't be so simple for AES192 and AES256
-    roundKey = nextExpandedKey;
+    `ifdef AES_192 // 192-bit key
+    
+        // see note at the top of this file
+
+    `else // 128-bit key
+
+        roundKey = nextExpandedKey;
+
+    `endif // `ifdef AES_192
+
+    `else // Expanding a 256-bit key
+
+        for (int i=1; i<=3; ++i)
+            nextExpandedKey.columns[i] = nextExpandedKey.columns[i-1] ^ prevExpandedKey.columns[i];
+
+        nextExpandedKey.columns[4] = SubBytes_4(nextExpandedKey.columns[3]) ^ prevExpandedKey.columns[4];
+
+        for (int i=5; i<=7; ++i)
+            nextExpandedKey.columns[i] = nextExpandedKey.columns[i-1] ^ prevExpandedKey.columns[i];
+
+        // see note at the top of this file
+
+    `endif // `ifndef AES_256
 
 end
-
-function automatic keyColumn_t genFirstColumn(input integer round, keyColumn_t prevFirst, prevLast);
-
-    keyColumn_t out;
-
-    out = prevFirst ^ ApplyRcon(round, SubBytes_4(Rot(prevLast)));
-
-    return out;
-
-endfunction
-
 
 function automatic keyColumn_t ApplyRcon(input integer round, keyColumn_t in);
 
